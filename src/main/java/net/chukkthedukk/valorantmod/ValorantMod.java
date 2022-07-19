663a023fbd0c96b367b67126010e6a6029b1e0c6
BuildTools <unconfigured@null.spigotmc.org>
2022-07-19 14:45:40 -0600

    Added Standard Classic

--- src/main/java/net/chukkthedukk/valorantmod/ValorantMod.java
+++ src/main/java/net/chukkthedukk/valorantmod/ValorantMod.java
@@ -1,14 +1,18 @@
 package net.chukkthedukk.valorantmod;
 
+import net.chukkthedukk.valorantmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ValorantMod implements ModInitializer {
+	public static final String MOD_ID = "valorantmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger("valorantmod");
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
+
 	}
 }
